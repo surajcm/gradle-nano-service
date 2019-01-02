ab5afd76e7f3241b4216acbf124397e5102e77bf
Suraj Muraleedharan <v-smuraleedharan@expedia.com>
2019-01-02 10:16:19 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# gradle nano service
+
+This is a general purpose re-usable repository for gradle applications
+
---
+++ src/main/java/com/expedia/nano/NanoMain.java
@@ -0,0 +1,10 @@
+package com.expedia.nano;
+
+
+public class NanoMain {
+
+	public static void main(String args[]) {
+		System.out.println("Hello world !!!");
+	}
+
+}
